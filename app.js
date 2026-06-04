ac97a2e7015bcaad1f8d1af985fb81373f3a10c4
Petrosyan1488 <240120@turan-edu.kz>
2026-06-05 01:55:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://myratwuodgfzpmmnnzjt.supabase.co";
 const SUPABASE_KEY = "sb_publishable_Ic-pUV5WjxAdXnijymzQTA_1IwRQ6Ss";
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const roomsGrid = document.getElementById('rooms-grid');
 const roomForm = document.getElementById('room-form');
